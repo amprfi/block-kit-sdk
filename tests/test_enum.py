d386ee8744d56a44ea59f3e92df6eee20df4f1fe
Chris Georgen <c-georgen@users.noreply.github.com>
2025-06-20 16:24:13 -0500

    adds enum tests

---
+++ tests/test_enum.py
@@ -0,0 +1,24 @@
+import pytest
+from blocks.models import FeeType, RecurringInterval, FeeCurrency
+
+def test_fee_type_enum():
+    assert FeeType.FIXED_ONE_TIME == "fixed_one_time"
+    assert FeeType.FIXED_RECURRING == "fixed_recurring"
+
+def test_recurring_interval_enum():
+    assert RecurringInterval.MONTHLY == "monthly"
+    assert RecurringInterval.QUARTERLY == "quarterly"
+    assert RecurringInterval.ANNUALLY == "annually"
+
+def test_fee_currency_enum():
+    assert FeeCurrency.DAI == "DAI"
+    assert FeeCurrency.USDC == "USDC"
+    assert FeeCurrency.USDT == "USDT"
+    assert FeeCurrency.AMPR == "AMPR"
+    assert FeeCurrency.ETH == "ETH"
+    assert FeeCurrency.BTC == "BTC"
+    assert FeeCurrency.SOL == "SOL"
+
+def test_number_of_accepted_currencies():
+    expected_number_of_currencies = 6
+    assert len(FeeCurrency) == expected_number_of_currencies
